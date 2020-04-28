f67cb8204112ea96f5da00eb72cdf7c2d6380897
tkarasov <tkarasov@gmail.com>
2020-04-28 15:24:17 +0200

    new 16S for Kemen and RF

--- code/16S/dominant_phylotypes.R
+++ code/16S/dominant_phylotypes.R
@@ -9,9 +9,9 @@ library(DESeq2)
 ####################################################################################
 
 load("/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_GP1000.rds")
-load("/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_ITS_GP1000.rds")
+#load("/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_ITS_GP1000.rds")
 #subset to only athaliana
-GP_athal <- subset_samples(GP1000, Species == "Ath")
+GP_athal <- subset_samples(GP1000, Host_Species == "Ath")
 
 
 ####################################################################################
@@ -34,21 +34,21 @@ GP_at15_all = prune_taxa(colnames(otu_table(GP_at15)), GP1000)
 save(GP_at15_all, file = "/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_GP1000_at15.rds")
 
 
-####################################################################################
-# Subset ITS to ASVs present in at least 5% of samples
-####################################################################################
-otus = as.matrix(otu_table(GP_ITS_athal))
-
-is_zero = function(x){if(x==0)return(0)else return(1)}
-
-is_there_otus <- apply(otus, 1:2, FUN = is_zero)
-
-keep_otus <- which(colSums(is_there_otus)/dim(is_there_otus)[1] >= 0.05)
-
-GP_at15 <- prune_taxa(names(keep_otus), GP_athal)
-
-GP_Pseud <- subset_taxa(GP_at15, Genus=="Pseudomonas")
-
-GP_at15_all = prune_taxa(colnames(otu_table(GP_at15)), GP1000)
-
-save(GP_at15_all, file = "/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_ITS_GP1000_at15.rds")
+# ####################################################################################
+# # Subset ITS to ASVs present in at least 5% of samples
+# ####################################################################################
+# otus = as.matrix(otu_table(GP_ITS_athal))
+# 
+# is_zero = function(x){if(x==0)return(0)else return(1)}
+# 
+# is_there_otus <- apply(otus, 1:2, FUN = is_zero)
+# 
+# keep_otus <- which(colSums(is_there_otus)/dim(is_there_otus)[1] >= 0.05)
+# 
+# GP_at15 <- prune_taxa(names(keep_otus), GP_athal)
+# 
+# GP_Pseud <- subset_taxa(GP_at15, Genus=="Pseudomonas")
+# 
+# GP_at15_all = prune_taxa(colnames(otu_table(GP_at15)), GP1000)
+# 
+# save(GP_at15_all, file = "/ebio/abt6_projects9/pathodopsis_microbiomes/data/figures_misc/OTUtab_ITS_GP1000_at15.rds")
