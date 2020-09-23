4832a8e92cc92c67ab3384cb2a7c53bdc3a958b1
tkarasov <tkarasov@gmail.com>
2020-09-23 17:18:35 -0600

    Update process_step3_analyses_16S.sh

--- code/16S/process_step3_analyses_16S.sh
+++ code/16S/process_step3_analyses_16S.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-
+# this script is the overall analysis script to generate the many analyses
 scripts=/ebio/abt6_projects9/pathodopsis_microbiomes/pathodopsis_git/code/
+
 # Make the generic OTU table
+# 16S
 $scripts/amplicon_general/after_dada2_make_otutable_generic.R
 
-# compare plant species
+# ITS
+$scripts/amplicon_general/after_dada2_make_otutable_generic_ITS.R
+
+# Compare plant species
 $scripts/climate/hierarchical_multiple_testing.R
 
 # PCA on the subsetted microbiome
-
-
 
 # Kriging on the microbiome
 $scripts/climate/kmeans_clustering.R
 
 # Random forest on the microbiome
 $scripts/climate/random_forest_iterative.R
+
+# Plant genotype comparisons to show differences in Fst of immune genes
+$scripts/plant_genotype/IBS.R
+
+# Plant health comparisons
+$scripts/plant_genotype/plant_health.R
