02df682970f77eb9aeac86a1a136750c31eb9b10
tkarasov <tkarasov@gmail.com>
2023-08-10 17:35:35 -0600

    Update run_gcta.sh
    running gcta so that I can get pca stuff

--- code/plant_genotype/run_gcta.sh
+++ code/plant_genotype/run_gcta.sh
@@ -1,5 +1,13 @@
 ###### THis script is meant to calculate the kinship matrix and variance explained by genotype
 cd /ebio/abt6_projects9/pathodopsis_microbiomes/data/
+gcta=/ebio/abt6_projects9/pathodopsis_microbiomes/Programs/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1
+#make a grm
+# https://yanglab.westlake.edu.cn/software/gcta/#MakingaGRM
+# poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_chlo.vcf.recode_geno0.3
+$gcta --bfile poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_chlo.vcf.recode_geno0.3  --thread-num 5 --out test
+
+# Input the GRM file and output the first 20 eigenvectors for a subset of individuals
+$gcta  --grm test --keep test.indi.list  --pca 20  --out test
 
 
 
@@ -63,7 +71,7 @@ plink --vcf   poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_ch
 
 gemma -gk 1 -maf 0.01 -bfile /ebio/abt6_projects9/pathodopsis_HpA/host/pathodopsis_Microbiome/poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_chlo.vcf.TK \
  	-a /ebio/abt6_projects9/pathodopsis_HpA/host/pathodopsis_Microbiome/postVcf/poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02.vcf.map \
- 	-p /ebio/abt6_projects9/pathodopsis_microbiomes/data/phenotype2.txt -o ex 
+ 	-p /ebio/abt6_projects9/pathodopsis_microbiomes/data/phenotype2.txt -o ex2 
 
 
 
