e629ac20429a6d5eec82eafdf72d10031859aad7
tkarasov <tkarasov@gmail.com>
2023-08-14 14:04:23 -0600

    Update run_gcta.sh

--- code/plant_genotype/run_gcta.sh
+++ code/plant_genotype/run_gcta.sh
@@ -4,6 +4,7 @@ gcta=/ebio/abt6_projects9/pathodopsis_microbiomes/Programs/gcta-1.94.1-linux-ker
 #make a grm
 # https://yanglab.westlake.edu.cn/software/gcta/#MakingaGRM
 # poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_chlo.vcf.recode_geno0.3
+
 $gcta --bfile poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_chlo.vcf.recode_geno0.3  --thread-num 5 --out test --make-grm
 
 # Input the GRM file and output the first 20 eigenvectors for a subset of individuals
@@ -11,6 +12,14 @@ $gcta  --grm test --pca 20  --out test
 #--keep test.indi.list 
 
 # Estimate variance explained by genotype
+$gcta --reml --grm test --pheno phenotype3.txt --out test
+# Summary result of REML analysis (phenotype is loading on PC1):
+# Source  Variance        SE
+# V(G)    0.010033        0.001269
+# V(e)    0.009107        0.001430
+# Vp      0.019140        0.001290
+# V(G)/Vp 0.524175        0.062331
+
 
 # Take the top 20 eigen vectors and include as covariates in the model for anova in vegan.
 
