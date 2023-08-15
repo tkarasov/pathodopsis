d8d7e0b0f776a58f09577c2911df8cea7c5d802f
tkarasov <tkarasov@gmail.com>
2023-08-15 14:49:44 -0600

    Update run_gcta.sh
    Ran gcta with MDS2 also

--- code/plant_genotype/run_gcta.sh
+++ code/plant_genotype/run_gcta.sh
@@ -11,7 +11,7 @@ $gcta --bfile poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02_no_mit_no_ch
 $gcta  --grm test --pca 20  --out test 
 #--keep test.indi.list 
 
-# Estimate variance explained by genotype
+# Estimate variance explained by genotype. phenotype3 is MDS1
 $gcta --reml --grm test --pheno phenotype3.txt --out test
 # Summary result of REML analysis (phenotype is loading on PC1):
 # Source  Variance        SE
@@ -20,15 +20,19 @@ $gcta --reml --grm test --pheno phenotype3.txt --out test
 # Vp      0.019140        0.001290
 # V(G)/Vp 0.524175        0.062331
 
+# Estimate variance explained by genotype. phenotype3 is MDS1, take second phenotype --mpheno 2
+$gcta --reml --grm test --pheno phen_MDS1_MDS2.txt --out test_MDS2 --mpheno 2
+#Summary result of REML analysis:
+# Source	Variance	SE
+# V(G)	0.008081	0.001484
+# V(e)	0.015517	0.002140
+# Vp	0.023598	0.001643
+# V(G)/Vp	0.342441	0.065775
 
-# Take the top 20 eigen vectors and include as covariates in the model for anova in vegan.
+# Take the top 5 eigen vectors and include as covariates in the model for anova in vegan.
 # eigenvectors are in test.eigenvec. First two columns are individual and family ID. These can be fed into R with the other microbe data
-# now run vegan
+# now run glm with MDS1 and MDS2 and vegan with whole matrix. 
 run_vegan_reml.R
 
 
-#  
-#  gemma -gk 1 -maf 0.1 -bfile poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02.vcf \
-#  -a /ebio/abt6_projects9/pathodopsis_HpA/host/pathodopsis_Microbiome/postVcf/poolsGVCF.filtered_snps_final.PASS.bi.cleaned.maf0_02.vcf.map \
-#  -p /ebio/abt6_projects9/pathodopsis_microbiomes/data/phenotype2.txt -o ex
-#  
+
